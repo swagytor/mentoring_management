cbe5c48753e5bada8216612b6af54331e027ab30
Игнатенко Алексей <as.ignatenko@idaproject.com>
2026-03-19 02:30:20 +0300

    Add simplejwt view controllers

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,17 +16,24 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from config import settings
 
+v0_urls = [
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v0/", include((v0_urls, "v0"), namespace="v0")),
 ]
 
 swagger_urs = [
