329d36c633cbd021eddbebe713c4edec7675e522
Игнатенко Алексей <as.ignatenko@idaproject.com>
2026-03-19 02:29:17 +0300

    Add drf-spectacular scheme

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,7 +17,31 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+from config import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+swagger_urs = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
+
+if not settings.PROD:
+    urlpatterns += swagger_urs
