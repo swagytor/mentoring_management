677bf9021c5ff779c9f066532538f431fbe05cec
Игнатенко Алексей <swagytor@yandex.ru>
2026-03-20 13:38:40 +0300

    Add student add/remove service classes

--- app/users/services/__init__.py
+++ app/users/services/__init__.py
@@ -1,2 +1,4 @@
 from .register_user import RegisterUserService
 from .update_user import UpdateUserService
+from .add_student import AddStudentService
+from .remove_student import RemoveStudentService
