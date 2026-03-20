55d4056039b6f692d68849b0aaf289aec7e98774
Игнатенко Алексей <swagytor@yandex.ru>
2026-03-20 03:56:23 +0300

    Add update and logout requests for User

--- app/users/views.py
+++ app/users/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins, status, views
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from config.permissions import IsOwner
 from users.models import User
@@ -11,7 +12,7 @@ from users.serializers import (
     UpdateUserSerializer,
     RetrieveUserSerializer,
 )
-from users.services import RegisterUserService
+from users.services import RegisterUserService, UpdateUserService
 
 
 class UserViewSet(
@@ -57,3 +58,28 @@ class UserViewSet(
         instance = self.get_object()
         serializer = self.get_serializer(instance, context={"request": request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance=instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        instance = UpdateUserService()(instance, serializer.validated_data)
+        serializer = RetrieveUserSerializer(instance, context={"request": request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserLogoutAPIView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["post"])
+    def logout(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
